4d28e4d4e1f16311f90abd139cd6a0100ca058a9
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2023-02-16 23:54:11 +0300

    Сформировал data class note с двумя свойствами и вывел их на экран приложения. b. Создал экземпляр класса note, вызвал copy и вывел значения скопированного класса на экран. c. Вывел значения цикла в консоль

--- app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
@@ -3,18 +3,37 @@ package com.example.kotlinlesson1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button_test)
-        button.setOnClickListener{
+        val noteOne = Note("Понедельник", "Заметка")
+        val noteTwo = noteOne.copy(title = "Среда")
+        val textViewOne: TextView = findViewById(R.id.textViewOne)
+        val textViewTwo: TextView = findViewById(R.id.textViewTwo)
 
+
+        val button: Button = findViewById(R.id.button_test)
+        button.setOnClickListener {
+            textViewOne.text = noteOne.title + " " + noteOne.description
+            textViewTwo.text = noteTwo.title + " " + noteTwo.description
+            consoleCycleOutput()
+
+
+        }
+    }
+
+    fun consoleCycleOutput() {
+        for (i in 1..5) {
+            Log.d("myLog", "$i consoleCycleOutput")
         }
     }
 }
---
+++ app/src/main/java/com/example/kotlinlesson1/Note.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinlesson1
+
+data class Note(val title: String, val description: String)
