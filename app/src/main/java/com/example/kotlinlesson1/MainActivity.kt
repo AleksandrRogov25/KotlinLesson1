f68654758efa2c9e2262002d64097221ac8830eb
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2023-02-16 22:57:17 +0300

    Добавил кнопку в разметку и повесил на неё clickListener в Activity

--- app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson1/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.kotlinlesson1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.kotlinlesson1.R
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button_test)
+        button.setOnClickListener{
+
+        }
     }
 }
